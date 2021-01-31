15aeb24458199b0fd766541555559fadf9e33407
bluefireoly <bluefireolymp@gmail.com>
2021-01-31 02:18:03 +0100

    Create CalculateBestSeatBetweenFriends.kt

---
+++ src/main/kotlin/CalculateBestSeatBetweenFriends.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val bob = readInts()
+    val erica = readInts()
+    println("${(bob[0] + erica[0]) / 2} ${(bob[1] + erica[1]) / 2}")
+}
+
+fun readInts() = readLine()!!.split(' ').map { it.toInt() }
