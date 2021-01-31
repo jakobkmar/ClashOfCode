7a5d2e5572ffec3d2e5f80ae743f143f868a6455
bluefireoly <bluefireolymp@gmail.com>
2021-01-31 13:43:13 +0100

    Update MonthSequenceGetFollowing.kt

--- src/main/kotlin/MonthSequenceGetFollowing.kt
+++ src/main/kotlin/MonthSequenceGetFollowing.kt
@@ -1,4 +1,4 @@
-val year = "JFMAMJJASOND".repeat(100)
+private val year = "JFMAMJJASOND".repeat(100)
 fun main() {
     val months = readLine()!!
     println(year.split(months)[2].firstOrNull() ?: 'F')
