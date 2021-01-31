91bec3db1f1ffa6e6a3bf0097c78afa5c0f8bd9d
bluefireoly <bluefireolymp@gmail.com>
2021-01-31 01:31:42 +0100

    Create ReplaceTemplatesWithEmojis.kt

---
+++ src/main/kotlin/ReplaceTemplatesWithEmojis.kt
@@ -0,0 +1,15 @@
+val emojis = mapOf(
+    ":slight_smile:" to ":)",
+    ":disappointed:" to ":(",
+    ":loud_laugh:" to "XD",
+    ":open_mouth:" to ":o",
+    ":stuck_out_tongue:" to ":p"
+)
+
+fun main() = println(readLine()!!.replaceAll(emojis))
+
+fun String.replaceAll(map: Map<String, String>): String {
+    var result = this
+    map.forEach { (t, u) -> result = result.replace(t, u) }
+    return result
+}
